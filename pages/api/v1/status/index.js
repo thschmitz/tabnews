2ccffa096e440cb04c98b9421829eceb9ab0492a
Thomas Schmitz <thomas.henrique.schmitz@gmail.com>
2025-02-09 21:33:24 -0300

    add  and

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+   const result = await database.query("SELECT 1+1 as sum;");
+   console.log(result.rows);
+   response.status(200).json({ chave: "valor" });
+}
+
+export default status;
