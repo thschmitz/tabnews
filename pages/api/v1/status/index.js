e79637efb488584c19193316358059d4d22152c8
Thomas Schmitz <thomas.henrique.schmitz@gmail.com>
2025-02-12 09:48:51 -0300

    Finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
-import database from "infra/database";
+import database from "infra/database.js";
 
 async function status(request, response) {
-   const result = await database.query("SELECT 1+1 as sum;");
-   console.log(result.rows);
-   response.status(200).json({ chave: "valor" });
+   const updatedAt = new Date().toISOString();
+   const version = await database.query("SHOW server_version;");
+
+   const databaseMaxConnection = await database.query("SHOW max_connections;");
+   const databaseMaxConnectionValue =
+      databaseMaxConnection.rows[0].max_connections;
+
+   const databaseName = process.env.POSTGRES_DB;
+
+   const databaseUsedConnectionsResult = await database.query({
+      text: "SELECT COUNT(*)::int as length FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+   });
+
+   const databaseUsedConnectionsValue =
+      databaseUsedConnectionsResult.rows[0].count; // Conexoes abertas nesse momento, seja do nosso backend ou nao
+
+   response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+         database: {
+            db_version: version.rows[0].server_version,
+            db_max_connections: parseInt(databaseMaxConnectionValue),
+            db_used_connections: databaseUsedConnectionsValue,
+         },
+      },
+   });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,3 +2,44 @@ test("GET to /api/v1/status should return 200", async () => {
    const response = await fetch("http://localhost:3000/api/v1/status");
    expect(response.status).toBe(200);
 });
+
+test("TEST update_at FROM RESPONSE BODY", async () => {
+   const response = await fetch("http://localhost:3000/api/v1/status");
+   const responseBody = await response.json();
+   expect(responseBody.updated_at).toBeDefined(); // Verifica se existe, e nao seu conteudo
+
+   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+   expect(responseBody.updated_at).toEqual(parsedUpdatedAt); // JS aceita null para criar essa data
+   // Faco uma conversao forçada da data real e comparo elas, pra evitar vir um null e passar na validacao de transformar para uma data ISO8601
+});
+
+test("TEST db_version FROM RESPONSE BODY", async () => {
+   const response = await fetch("http://localhost:3000/api/v1/status");
+
+   const responseBody = await response.json();
+   expect(responseBody.dependencies.database.db_version).toEqual("16.0");
+});
+
+test("TEST db_max_connections FROM RESPONSE BODY", async () => {
+   const response = await fetch("http://localhost:3000/api/v1/status");
+
+   const responseBody = await response.json();
+   expect(responseBody.dependencies.database.db_max_connections).toEqual(100);
+});
+
+test("TEST db_used_connections FROM RESPONSE BODY", async () => {
+   const response = await fetch("http://localhost:3000/api/v1/status");
+
+   const responseBody = await response.json();
+   expect(responseBody.dependencies.database.db_used_connections).toEqual(1);
+});
+
+test("TEST SENDING databaseName PARAMETER FROM REQUEST HEADER", async () => {
+   await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+});
+
+/*test("TEST TRYING TO BREAK DOWN THE DATABASE BEING", async () => {
+   await fetch(
+      "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+   );
+});*/
